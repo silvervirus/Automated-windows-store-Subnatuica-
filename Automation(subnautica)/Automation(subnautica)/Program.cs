f2432d9d5b3a2cff92bd4c82b43cd63a0f19b1cc
doug gray <pred102@hotmail.com>
2020-08-02 12:56:21 -0700

    Added support for Microsoft Store

--- Automation(subnautica)/Automation(subnautica)/Program.cs
+++ Automation(subnautica)/Automation(subnautica)/Program.cs
@@ -1,67 +1,75 @@
 ﻿using System;
 using System.Threading;
-using System.Security.Permissions;
-using System.Security.Principal;
 using System.Diagnostics;
-
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Management;
+using System.Windows.Forms;
+
+using System.IO.Compression;
+
+
 
 class ProcessDemo
 {
     
-    
 
     public static void Main()
     {
-       
+
         Process notePad = Process.Start("ms-xbl-38616e6e:\\");
         Console.WriteLine("Started Subnautica process Id = " + notePad.Id);
         Console.WriteLine(notePad.MainModule.FileName);
-        System.Threading.Thread.Sleep(3000);
+        System.Threading.Thread.Sleep(10000);
+
         if (notePad.MainModule.FileName.Contains("WindowsApps"))
         {
+           
             ProcessStartInfo startInfo = new ProcessStartInfo("c:\\users\\pred1\\Desktop\\UWPInjector.exe");
             startInfo.CreateNoWindow = true;
-             startInfo.Arguments = "-p " + notePad.Id;
+            startInfo.Arguments = "-p " + notePad.Id;
             Process.Start(startInfo);
 
-            FileSystemWatcher watcher;
 
-            void watch()
+            var perfCounter = new PerformanceCounter("Process", "% Processor Time", "UWPInjector");
+
+            // Initialize to start capturing
+            perfCounter.NextValue();
+
+            for (int i = 0; i < 200; i++)
             {
-                watcher = new FileSystemWatcher();
-                watcher.Path = @"%AppData%\Local\Packages\UnknownWorldsEntertainmen.GAMEPREVIEWSubnautica_bh1f6rvenfkm2\TempState\DUMP";
-                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
-                                       | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-                watcher.Filter = "*.*";
-                watcher.Changed += new FileSystemEventHandler(OnChanged);
-                watcher.EnableRaisingEvents = true;
+                // give some time to accumulate data
+                Thread.Sleep(1000);
+
+                float cpu = perfCounter.NextValue() / Environment.ProcessorCount;
+              
+                Console.WriteLine("UWPInjector CPU: " + cpu);
+                Thread.Sleep(3000);
+                if (cpu == 0)
+                {
+                    try
+                    {
+                        foreach (Process proc in Process.GetProcessesByName("UWPInjector.exe"))
+                        {
+                            proc.Kill();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                } 
+                    
+
+
+
             }
 
-            void OnChanged(object source, FileSystemEventArgs e)
-            {
-                string sourceFile = @"%AppData%\Local\Packages\UnknownWorldsEntertainmen.GAMEPREVIEWSubnautica_bh1f6rvenfkm2\TempState\DUMP";
-                string destinationFile = @"C:\Games\Subnautica";
-
-                // To move a file or folder to a new location:
-                System.IO.File.Move(sourceFile, destinationFile);
-
-                // To move an entire directory. To programmatically modify or combine
-                // path strings, use the System.IO.Path class.
-                System.IO.Directory.Move(@"%AppData%\Local\Packages\UnknownWorldsEntertainmen.GAMEPREVIEWSubnautica_bh1f6rvenfkm2\TempState\DUMP", @"C:\Games\Subnautica");
-            }
-
-
-
-
+                       
 
 
 
 
         }
+
+
     }
 }
-
